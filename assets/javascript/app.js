1441b2b98745a1e5dfce26411878d9bddc7d84ea
bking1989 <bradley_king@me.com>
2018-11-12 17:24:54 -0700

    Began working on basic HTML and CSS structure w/ JS for opening screen

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// Global variables
+
+$(document).ready(function() {
+    // When the opening screen is clicked on, it disappears
+    $("#openingScreen").on("click", function() {
+        $(this).addClass("hide");
+    });
+});
